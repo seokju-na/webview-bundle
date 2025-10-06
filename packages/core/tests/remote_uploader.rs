7a3105f4559dbe46143a49d6bcad1282c570633c
Seokju Na <seokju.me@gmail.com>
2025-10-06 17:41:27 +0900

    napi 3.3.0

--- packages/core/tests/remote_uploader.rs
+++ packages/core/tests/remote_uploader.rs
@@ -1,28 +1,37 @@
-#[skipif::skip_if(missing_env(MINIO_TESTING_URL))]
 #[cfg(feature = "remote-uploader-s3")]
-#[tokio::test]
-async fn s3_smoke() {
+mod remote_uploader_tests {
   use std::path::PathBuf;
   use webview_bundle::remote::uploader::S3Uploader;
+  use webview_bundle::remote::uploader::Uploader;
   use webview_bundle::{Bundle, BundleReader, Reader};
 
-  let s3 = S3Uploader::builder()
-    .endpoint(std::env::var("MINIO_TESTING_URL").unwrap())
-    .bucket("webview-bundle")
-    .access_key_id("minio_testing")
-    .secret_access_key("minio_testing")
-    .build()
+  #[tokio::test]
+  async fn s3_smoke() {
+    if std::env::var("MINIO_TESTING_URL").is_err() {
+      return;
+    }
+
+    let s3 = S3Uploader::builder()
+      .endpoint(std::env::var("MINIO_TESTING_URL").unwrap())
+      .bucket("webview-bundle")
+      .access_key_id("minio_testing")
+      .secret_access_key("minio_testing")
+      .build()
+      .unwrap();
+    let mut reader = std::fs::File::open(
+      PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("tests")
+        .join("fixtures")
+        .join("bundles")
+        .join("builtin")
+        .join("nextjs")
+        .join("nextjs_1.0.0.wvb"),
+    )
     .unwrap();
-  let mut reader = std::fs::File::open(
-    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-      .join("tests")
-      .join("fixtures")
-      .join("nextjs.wvb"),
-  )
-  .unwrap();
-  let bundle = Reader::<Bundle>::read(&mut BundleReader::new(&mut reader)).unwrap();
-  s3.upload_bundle("nextjs", "1.0.0", &bundle)
-    .await
-    .expect("fail to upload bundle");
-  // TODO: add remote testing
+    let bundle = Reader::<Bundle>::read(&mut BundleReader::new(&mut reader)).unwrap();
+    s3.upload_bundle("nextjs", "1.0.0", &bundle)
+      .await
+      .expect("fail to upload bundle");
+    // TODO: add remote testing
+  }
 }
